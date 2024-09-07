bce1af734a14a4721e31defd5d4536af94923ef5
marcos-rms <marcos_ramos91@hotmail.com>
2024-09-06 23:26:20 -0300

    Update animations of the nav scroll buttons

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,35 +1,82 @@
+<script setup>
+function changeItem(x) {  
+  for (let i = 1; i < 6; i++) {
+    document.getElementById("item" + i).attributes.getNamedItem("class").value = "diamond";
+  }
+  document.getElementById("item" + x).attributes.getNamedItem("class").value = "square";
+}
+</script>
+
 <template>
-  <!-- <ul>
-    <li class="square"></li>
-    <li class="diamond"></li>
-    <li class="diamond"></li>
-    <li class="diamond"></li>
-  </ul> -->
+  <nav>
+    <ul>
+      <li>
+        <button id="item1" @click="changeItem(1)" class="square"></button>
+      </li>
+      <li>
+        <button id="item2" @click="changeItem(2)" class="diamond"></button>
+      </li>
+      <li>
+        <button id="item3" @click="changeItem(3)" class="diamond"></button>
+      </li>
+      <li>
+        <button id="item4" @click="changeItem(4)" class="diamond"></button>
+      </li>
+      <li>
+        <button id="item5" @click="changeItem(5)" class="diamond"></button>
+      </li>
+      </ul>
+  </nav>
 </template>
 
 <style scoped>
-  /* ul {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+  
+  ul {
     list-style-type: none;
-    position: fixed;
-    bottom: 55px;
+    /* bottom: 55px;
     right: 95.6px;
-    margin: auto;
+    margin: auto; */
+  }
+  #item1 {
+    position: fixed;
+    bottom: 163px;
+  }
+  #item2 {
+    position: fixed;
+    bottom: 136px;
+  }
+  #item3 {
+    position: fixed;
+    bottom: 104px;
+  }
+  #item4 {
+    position: fixed;
+    bottom: 77px;
+  }
+  #item5{
+    position: fixed;
+    bottom: 50px;
   }
   .diamond {
     width: 8px;
     height: 8px;
+    right: 95.6px;
     background-color: black;
     transform: rotate(45deg);
-    margin-top: 27px;
+    border: none;
   }
   .square {
+    position: fixed;
     width: 14px;
     height: 14px;
+    right: 92.7px;
     border: 2px solid black;
     background-color: transparent;
-    transform: none;
-  } */
+  }
+  button {
+    cursor: pointer;
+  }
+  button:focus {
+    transition: transform .35s ease;
+  }
 </style>
